d046635c2cd1ffff17e2bf0c1a0f8a674e2a2711
Mario Magomedov <webdevelope2017@gmail.com>
2017-10-04 17:21:24 +0300

    on click evetn multiple

---
+++ main.js
@@ -0,0 +1,34 @@
+$(function () {
+  var link = $('.menu ul li > a');
+
+  link.on('click', function(e){
+    e.preventDefault();
+
+    if($(e.target).is('#about')){
+      $('html,body').stop().animate({ scrollTop: $('.section2').offset().top }, 1000);
+    }
+
+    if($(e.target).is('#work')){
+      $('html,body').stop().animate({ scrollTop: $('.section3').offset().top }, 1000);
+    }
+
+    if($(e.target).is('#progress')){
+      $('html,body').stop().animate({ scrollTop: $('.section6').offset().top }, 1000);
+    }
+
+    if( $(e.target).is('#services') ){
+      $('html, body').animate({scrollTop:
+      $('.section7').offset().top}, 1000);
+    }
+
+    if( $(e.target).is('#testimonials') ){
+      $('html, body').animate({scrollTop:
+      $('.section8').offset().top}, 1000);
+    }
+
+    if( $(e.target).is('#contact') ){
+      $('html, body').animate({scrollTop:
+      $('.section10').offset().top}, 1000);
+    }
+  });
+});
